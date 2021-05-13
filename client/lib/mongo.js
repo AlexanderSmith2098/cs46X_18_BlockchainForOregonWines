236a94b46744ea7a4e1eee12400d736789db6df8
paynem <mpayne677@gmail.com>
2021-05-13 09:41:29 -0700

    Added Sonia's styling.  Corrected Docker crap.

--- client/lib/mongo.js
+++ client/lib/mongo.js
@@ -1,6 +1,6 @@
 // This file either reads environment variables or uses hardcoded values.  This is the information that the client needs to connect to the mongodb that is setup on docker.
 const mongoHost = process.env.MONGO_HOST || "localhost"
-const mongoPort = process.env.MONGO_PORT || "27017"
+const mongoPort = process.env.MONGO_PORT || "3300"
 const mongoUser = process.env.MONGO_USER || "winery"
 const mongoPassword = process.env.MONGO_PASSWORD || "pass"
 const mongoDBName = process.env.MONGO_DB_NAME || "oregon-wines"
